ed14f7e01f33a77fe5964437e2d1837d778a4d2c
kellyMiy <kellyaraujoads@gmail.com>
2020-10-09 21:48:13 -0300

    Concluido implementacao do mapa.

--- app/src/main/java/com/example/projetomaps/MainActivity.java
+++ app/src/main/java/com/example/projetomaps/MainActivity.java
@@ -1,14 +1,97 @@
 package com.example.projetomaps;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LocationManager locationManager;
+    private LocationListener localionListener;
+    private static final int REQUEST_CODE_GPS = 1001;
+    private TextView locationTextView;
+    private double latitudeAtual;
+    private double longitudeAtual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        localionListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                double lat = location.getLatitude();
+                double lon = location.getLongitude();
+                latitudeAtual = lat;
+                longitudeAtual = lon;
+                locationTextView.setText(String.format("Lat: %f, Long: %f, lat, lon"));
+            }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+
+        };
+}
+@Override
+protected void onStart(){
+    super.onStart();
+    if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)==
+    PackageManager.PERMISSION_GRANTED){
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,localionListener);
+    }else{
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE_GPS);
+    }
+    }
+    @Override
+    public  void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if(requestCode == REQUEST_CODE_GPS){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,localionListener);
+                }
+            }
+            else {
+                Toast.makeText(this,getString(R.string.no_gps_no_app),Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        locationManager.removeUpdates(localionListener);
+    }
+
+    public void carregarMapa(View view){
+        Uri gmmIntentUri = Uri.parse(String.format("geo: %f, %f?q=restaurantes",latitudeAtual, longitudeAtual));
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
     }
 }
